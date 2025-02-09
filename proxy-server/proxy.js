4055b86497b19482c6f89264667c9203cec3c495
Nick <nicholasterehin@gmail.com>
2025-02-08 19:23:58 -0500

    Added serverEnabled variable

--- proxy-server/proxy.js
+++ proxy-server/proxy.js
@@ -9,6 +9,7 @@ let fetch;
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+let serverEnabled = true;
 
 const clientID = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
